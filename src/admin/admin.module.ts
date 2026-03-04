568761ba289e8a13b7dc3328f3c66a1b20243331
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2026-03-05 02:28:42 +0600

    change in folder structure

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from 'src/entities/user.entity';
+import { UserEntity } from 'src/common/entities/user.entity';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AdminModule } from './modules/admin/admin.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
     imports: [
