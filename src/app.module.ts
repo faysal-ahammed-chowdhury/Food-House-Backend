fe4fe47626a25b977d5dc1cdec0a4f83c42a399a
Faysal Ahammed Chowdhury <faysalahammedchowdhury01@gmail.com>
2026-05-12 22:01:27 +0600

    added env and pusherjs admin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
@@ -10,25 +11,29 @@ import { RestaurantModule } from './restaurant/restaurant.module';
         AdminModule,
         AuthModule,
         RestaurantModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'root',
-            database: 'foodhouse',
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASS,
+            database: process.env.DB_NAME,
             autoLoadEntities: true,
             synchronize: true,
             // dropSchema: true,
         }),
         MailerModule.forRoot({
             transport: {
-                host: 'smtp.gmail.com',
-                port: 587,
+                host: process.env.MAILER_HOST || '',
+                port: process.env.MAILER_PORT || '',
                 secure: false,
                 auth: {
-                    user: 'faysal.a.chowdhury.1@gmail.com',
-                    pass: 'jbpafbyzkunplhba',
+                    user: process.env.MAILER_AUTH_USER,
+                    pass: process.env.MAILER_AUTH_PASS,
                 },
             },
         }),
